295de93543b1bfe7f6797a949744221fd811622e
Yuda <yuda10aditya@gmail.com>
2021-01-30 16:31:39 +0700

    modified Task

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -19,6 +20,13 @@ function App() {
       reminder: true
     }
   ])
+
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks,  newTask])
+  }
 
   // Delete Task
   const deleteTask = (id) => {
@@ -37,8 +45,8 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ?
         (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />)
         :
